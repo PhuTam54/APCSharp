08aaa0e1e11c10ad791f3afbc9d4f9fe2ee35f62
PhuTam54 <phutamytb@gmail.com>
2023-08-17 15:18:40 +0700

    Add assignment3

--- Program.cs
+++ Program.cs
@@ -3,10 +3,55 @@ using System.Collections.Generic;
 using T2210A_CSharp.assignment1;
 using T2210A_CSharp.demo3;
 using T2210A_CSharp.assignment2;
+using T2210A_CSharp.demo4;
+using T2210A_CSharp.assignment3;
 
 public class Program
 {
     public static void Main(string[] args)
+    {
+        BankAccount ba = new BankAccount(500000);
+        ba.Deposit(200000);
+        ba.Withdraw(150000);
+    }
+
+    public static void Main6(string[] args)
+    {
+        VoidStringDelegate vsd = new VoidStringDelegate(DemoDelegate.Goodbye);
+        // DemoDelegate.Goodbye("Hello world");
+        vsd("Hello world");
+
+        VoidStringDelegate vsd1 = new VoidStringDelegate(new DemoDelegate().SayHello);
+        vsd1("Hello world 1");
+
+        var t2 = new DemoDelegate();
+        t2.SayHello("Hello");
+        DemoDelegate.Goodbye("Goodbye");
+
+        VoidStringDelegate vsd2 = new VoidStringDelegate(new DemoDelegate().SayHello);
+        vsd2 += DemoDelegate.Goodbye;
+        vsd2 += DemoDelegate.Goodbye;
+
+        vsd2("T2210A");
+
+        DisplayMessage("Hello world 2", DemoDelegate.Goodbye);
+
+        VoidStringDelegate vsd3 = delegate (string s)  // anonymous method
+        {
+            Console.WriteLine(s);
+        };
+        VoidStringDelegate vsd4 = s => Console.WriteLine(s); // lambda expression
+
+        vsd3("ABC");
+        vsd4("XYZ");
+    }
+
+    static void DisplayMessage(string message, VoidStringDelegate callback)
+    {
+        callback(message);
+    }
+
+    public static void Main5(string[] args)
     { 
         PhoneBook phoneBooks = new PhoneBook();
 
---
+++ assignment3/BankAccount.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2210A_CSharp.assignment3
+{
+    public delegate void UpdateBalanceHandler(double balance);
+    internal class BankAccount
+    {
+        private double balance;
+        public event UpdateBalanceHandler BalanceChanged;
+
+        public BankAccount(double balance)
+        {
+            this.balance = balance;
+            BalanceChanged += NotifyChangedbalance;
+        }
+        public BankAccount()
+        {
+            BalanceChanged += NotifyChangedbalance;
+        }
+        public double Balance
+        {
+            get => balance;
+            set
+            {
+                balance = value; // Kiểm tra xem event BalanceChanged có null hay không trước khi gọi nó.
+                BalanceChanged?.Invoke(balance);
+            }
+            }
+
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                Balance += amount;
+                BalanceChanged(amount);
+            }
+            else
+            {
+                Console.WriteLine("Failed.");
+            }
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount < Balance)
+            {
+                Balance -= amount;
+                BalanceChanged(amount);
+            }
+            else
+            {
+                Console.WriteLine("Failed.");
+            }
+        }
+
+        public void NotifyChangedbalance(double balance)
+        {
+            Console.WriteLine("Balance changed: " + balance);
+        }
+    }
+}
